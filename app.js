e16e37e1eb382b388b1bfb0dbdd30af4961810ec
adam hardie <adam.hardie13@gmail.com>
2020-06-25 16:24:14 +0100

    inital commit

---
+++ app.js
@@ -0,0 +1,10 @@
+/* This contains a bunch of methods which I will work on over time just for fun. */
+import {findAndReplace, splitAString, reverseAString, isPalindrome} from './stringMethods.js';
+
+
+
+console.log(findAndReplace("The Quick red fox", "red", "blue"));
+console.log("Split a string method: ", splitAString("Split String Here"));
+console.log("Reversed String method: ", reverseAString("This is a Reversed String"));
+console.log("Is Reversed String === Original String:", isPalindrome("demo"));
+
---
+++ stringMethods.js
@@ -0,0 +1,20 @@
+const splitAString = (str) => {return str.split('')};
+
+const reverseAString = (str) => {return splitAString(str).reverse().join('')};
+
+const isPalindrome = (str) => {
+    if (str === reverseAString(str)){
+        return true;
+    }else {
+        return false;
+    }
+}
+
+
+const findAndReplace = (originalString, textoToReplace, replacementText) => {
+   
+    return  originalString.replace(textoToReplace, replacementText);
+}
+
+export {splitAString, reverseAString, isPalindrome, findAndReplace};
+
