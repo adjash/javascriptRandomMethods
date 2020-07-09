564c602efad59eb1a6785c49cf1b5b5fcac94895
adam hardie <adam.hardie13@gmail.com>
2020-07-09 12:18:55 +0100

    matchstickhouse method and Can array be nested method

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 /* This contains a bunch of methods which I will work on over time just for fun. */
 import {findAndReplace, splitAString, reverseAString, isPalindrome} from './stringMethods.js';
-import { triangleArea, addAllUpTo } from './equationMethods.js';
+import { triangleArea, addAllUpTo, matchStickHouses, canArrayBeNested } from './equationMethods.js';
 
 
 console.log(findAndReplace("The Quick red fox", "red", "blue"));
@@ -9,3 +9,14 @@ console.log("Reversed String method: ", reverseAString("This is a Reversed Strin
 console.log("Is Reversed String === Original String:", isPalindrome("demo"));
 console.log(triangleArea(7,4));
 console.log(addAllUpTo(13));
+
+console.log(matchStickHouses(4))
+
+
+console.log(canArrayBeNested([1, 2, 3, 4], [0, 6]));
+
+console.log(canArrayBeNested([3, 1], [4, 0]));
+
+console.log(canArrayBeNested([9, 9, 8], [8, 9]));
+
+console.log(canArrayBeNested([1, 2, 3, 4], [2, 3]));
--- equationMethods.js
+++ equationMethods.js
@@ -6,4 +6,27 @@ const addAllUpTo = (countTo) => {
     return (!countTo) ? 0 : countTo + addAllUpTo(countTo-1)
 }
 
-export {triangleArea, addAllUpTo}
+const matchStickHouses = (houseCount) => {
+    if (houseCount == 0) return 0;
+    let minihouse = 5;
+    let initialHouse = 6;
+    let demo = houseCount - 1;
+    let afterFirst = demo * minihouse;
+    let postFirst = afterFirst + initialHouse;
+    return postFirst
+}
+
+const canArrayBeNested = (arr1, arr2) => {
+    arr1.sort((a,b) => a-b);
+    arr2.sort((a,b) => a-b);
+    let min1 = arr1[0], min2 = arr2[0];
+    if (min1 > min2 && arr1.slice(-1) < arr2.slice(-1)) {
+        console.log('true')
+        return true
+    }else {
+        console.log('false');
+        return false;
+    }
+}
+
+export {triangleArea, addAllUpTo, matchStickHouses, canArrayBeNested}
